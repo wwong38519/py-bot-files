6bd8dbb26f843cb44cff9541dd2fac573a3c0309
wwong38519 <wwong38519@gmail.com>
2017-02-28 14:24:18 +0800

    Modify log handler to Timed Rotating File Handler

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#import bot
 import logging
 import logging.handlers
 
@@ -9,7 +8,7 @@ def setupLog():
     import config
     logger = logging.getLogger()
     logFormatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fileHandler = logging.handlers.RotatingFileHandler(config.logFile, encoding='utf-8')
+    fileHandler = logging.handlers.TimedRotatingFileHandler(config.logFile, encoding='utf-8', when='h', interval=1)
     fileHandler.setFormatter(logFormatter)
     fileHandler.setLevel(logging.DEBUG)
     consoleHandler = logging.StreamHandler()
